23db0a8c51c0dc39677c751d75eb49ec0d2f8f4f
Michel San Martin <mikeemmanuel91@gmail.com>
2025-06-19 23:07:17 -0300

    defined 1 function

--- algoritmos_tpi.py
+++ algoritmos_tpi.py
@@ -1,13 +1,34 @@
 # Trabajo practico integrador: Análisis de Algoritmos en Python 
 # Alumnos: Michel Emmanuel San Martin (mikeemmanuel91@gmail.com)
 
+import time
+# Se definen las funciones con el modulo time para ayudarnos a contar en las pruebas empiricas.
+def use_time():
+    used_time = time.time()
+    return used_time
+
+def time_dif(start_time, end_time):
+    total_time = end_time - start_time
+    print(f"El tiempo transcurrido en la ejecucion fue de: {total_time:.8f} segundos.")
+
 # Algoritmo_1
-def dos_numeros_bruto(lista, objetivo):
-    for i in range(len(lista)):
-        for j in range(i + 1, len(lista)):
-            if lista[i] + lista[j] == objetivo:
-                return (lista[i], lista[j])
-    return None
+def buscar_combinacion_bruto(lista, objetivo):
+    start = use_time()
+    print("Comienzo de Algoritmo_1.")
+    objetivo = objetivo.upper()
+
+    for nombre in lista:
+        if nombre.upper() == objetivo:
+            end=use_time()
+            time_dif(start, end)
+            print(f"El Pokemon ingresado es el numero #{lista.index(nombre) + 1}")
+            return True
+    
+    else:
+        end = use_time()
+        time_dif(start, end)
+        print("El nombre de pokemon ingresado no es valido o este no pertenece a la region de Kanto.")
+        return False
 
 # Algoritmo_2
 def dos_numeros_hashset(lista, objetivo):
@@ -18,3 +39,4 @@ def dos_numeros_hashset(lista, objetivo):
             return (complemento, num)
         vistos.add(num)
     return None
+
---
+++ main.py
@@ -0,0 +1,6 @@
+import lista_pokemon
+import algoritmos_tpi
+
+poke_busqueda = input("Por favor, ingrese el nombre de un pokemon: ")
+
+algoritmos_tpi.buscar_combinacion_bruto(lista_pokemon.lista, poke_busqueda)
